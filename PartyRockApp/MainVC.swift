9a021df2ea8f205074cd853970e13e0e76cd06d7
muzha93 <luka@lukas-MacBook-Pro.local>
2016-10-30 00:05:00 +0200

    dodano jeobjekt u table view

--- PartyRockApp/MainVC.swift
+++ PartyRockApp/MainVC.swift
@@ -8,20 +8,35 @@
 
 import UIKit
 
-class MainVC: UIViewController {
+class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
     
     var partyRocks = [PartyRock]()
     
     override func viewDidLoad() {
-        
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let p1 = PartyRock(imageURL: "http://tinypic.com/r/vwwj69/5", videoURL: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/t7hL0LrmBj8\" frameborder=\"0\" allowfullscreen></iframe>", videoTitle: "Smoke some weed")
+        partyRocks.append(p1)
+        tableView.delegate = self
+        tableView.dataSource = self
     }
-
+// recycle cells
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "PartyCell", for: indexPath) as? PartyCell{
+            
+            let partyRock = partyRocks[indexPath.row]
+            cell.updateUI(partyRock: partyRock)
+        } else {
+            return UITableViewCell()
+        }
+        return UITableViewCell()
+    }
     
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return partyRocks.count
+    }
 
 }
 
