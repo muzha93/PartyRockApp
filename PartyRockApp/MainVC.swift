245d67ba961d486cf809036666369cef052411b8
muzha93 <luka@lukas-MacBook-Pro.local>
2016-10-30 00:40:03 +0200

    skidanje slika s neta

--- PartyRockApp/MainVC.swift
+++ PartyRockApp/MainVC.swift
@@ -16,8 +16,17 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let p1 = PartyRock(imageURL: "http://tinypic.com/r/vwwj69/5", videoURL: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/t7hL0LrmBj8\" frameborder=\"0\" allowfullscreen></iframe>", videoTitle: "Smoke some weed")
+        let p1 = PartyRock(imageURL: "http://i44.tinypic.com/vwwj69.jpg", videoURL: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/t7hL0LrmBj8\" frameborder=\"0\" allowfullscreen></iframe>", videoTitle: "Smoke some weed")
+        let p2 = PartyRock(imageURL: "http://i41.tinypic.com/okcz1t.jpg", videoURL: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/t7hL0LrmBj8\" frameborder=\"0\" allowfullscreen></iframe>", videoTitle: "Smoke some weed")
+        let p3 = PartyRock(imageURL: "http://i39.tinypic.com/257elut.jpg", videoURL: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/t7hL0LrmBj8\" frameborder=\"0\" allowfullscreen></iframe>", videoTitle: "Smoke some weed")
+        let p4 = PartyRock(imageURL: "http://i43.tinypic.com/4vqtj6.jpg", videoURL: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/t7hL0LrmBj8\" frameborder=\"0\" allowfullscreen></iframe>", videoTitle: "Smoke some weed")
+        let p5 = PartyRock(imageURL: "http://i66.tinypic.com/1zlrb6h.jpg", videoURL: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/t7hL0LrmBj8\" frameborder=\"0\" allowfullscreen></iframe>", videoTitle: "Smoke some weed")
+
         partyRocks.append(p1)
+        partyRocks.append(p2)
+        partyRocks.append(p3)
+        partyRocks.append(p4)
+        partyRocks.append(p5)
         tableView.delegate = self
         tableView.dataSource = self
     }
--- PartyRockApp/PartyCell.swift
+++ PartyRockApp/PartyCell.swift
@@ -22,6 +22,19 @@ class PartyCell: UITableViewCell {
     func updateUI (partyRock: PartyRock) {
         videoTitle.text = partyRock.videoTitle
         //todo za sliku jos moras
+        // when downloading from internet asynchronous thread
+        let url = URL(string: partyRock.imageURL)!
+        // backgroung thread can make only operation
+        DispatchQueue.global().async {
+            do {
+                let data = try Data(contentsOf: url)
+                DispatchQueue.global().sync {
+                    self.videoPreviewImage.image = UIImage(data: data)
+                }
+            } catch {
+                
+            }
+        }
     }
     
     
